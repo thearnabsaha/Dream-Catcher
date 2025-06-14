7e9679225da8b90dfa231c06ed467fd4cd8b53a2
thearnabsaha <thearnabsaha201@gmail.com>
2025-06-14 10:07:49 +0530

    feat: initialize express backend with basic setup and health check endpoint
    - Added package.json for express backend with necessary dependencies and scripts.
    - Created index.ts for server setup, including middleware for logging, security, and parsing.
    - Implemented health check endpoint and user signup route with validation using Zod.
    - Configured TypeScript with tsconfig.json for express backend.
    
    feat: setup Tailwind CSS configuration for web app
    
    - Added Tailwind CSS configuration file for styling.
    - Updated VSCode settings to recognize Tailwind CSS config.
    
    feat: create backend-common package for shared configurations
    
    - Initialized backend-common package with dotenv configuration for JWT secret.
    - Added TypeScript configuration for backend-common.
    
    feat: create common package for shared types and validation schemas
    
    - Initialized common package with Zod validation schemas for user signup and signin.
    - Added TypeScript configuration for common package.
    
    feat: initialize db package with Prisma setup
    
    - Created db package with Prisma client and initial migrations for User and Todo tables.
    - Added TypeScript configuration for db package.
    - Implemented .gitignore for db package to exclude unnecessary files.
    
    feat: create UI input component
    
    - Developed reusable input component with styling and utility functions.

---
+++ apps/express-backend/src/index.ts
@@ -0,0 +1,56 @@
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 3001;
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import { SignInSchema, SignUpSchema } from '@workspace/common/types';
+import { JWT_SECRET } from '@workspace/backend-common/config'
+import { prisma } from '@workspace/db/client';
+const morganFormat = ':method :url :status :response-time ms';
+
+app.use(morgan(morganFormat));
+app.use(helmet());
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
+
+// app.options('*', cors({
+//   origin: process.env.CORS_ORIGIN,
+//   credentials: true,
+// }));
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
+app.get('/', (req, res) => {
+    res.send('hello from simple server :)');
+});
+app.get('/health', async (req, res) => {
+  const start = Date.now();
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: new Date(),
+    responseTime: `${Date.now() - start}ms`,
+  };
+  res.status(200).json(healthcheck);
+});
+app.post('/signup', async (req, res) => {
+  const result = SignUpSchema.safeParse(req.body);
+  if (!result.success) {
+    res.send(result.error.format());
+  } else {
+    const user=await prisma.user.createMany({
+      data:req.body
+    });
+    res.send(result);
+  }
+});
+app.listen(port, () => console.log('> Server is up and running on port: ' + port));
